1537c1d3e5b8e58ce7984623c52b59a2cb777703
dan-gryy <dan.gryschenko@gmail.com>
2025-09-21 23:46:49 +0300

    first commit

---
+++ lab3/methods.js
@@ -0,0 +1,23 @@
+//Функція, яка приймає об'єкт інтерфейсу і повертає масив з іменами методів та кількістю їх параметрів
+
+
+const methods = (iface) => {
+  const names = [];
+  for (const name in iface) {
+    const fn = iface[name];
+    if (typeof fn === 'function') {
+      names.push([name, fn.length]);
+    }
+  }
+  return names;
+};
+const iface = {
+  m1: x => [x],
+  m2: function (x, y) {
+    return [x, y];
+  },
+  m3(x, y, z) {
+    return [x, y, z];
+  }
+}
+console.log(methods(iface));
---
+++ lab3/random.js
@@ -0,0 +1,7 @@
+//Фукція, яка генерує випадкове ціле число в діапазоні від min до max (включно)
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * max);
+}   
+console.log(randomInt(0, 100)); 
+    
