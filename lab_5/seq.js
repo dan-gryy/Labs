7f5ebcdca977448b963af0cac0122491eea80a0d
dan-gryy <dan.gryschenko@gmail.com>
2025-09-26 23:28:13 +0300

    function edited

--- lab_5/seq.js
+++ lab_5/seq.js
@@ -15,3 +15,7 @@ function seq(...args) {
 console.log(seq((x) => x + 7)((x) => x * 2)(5));
 console.log(seq((x) => x + 2)((x) => x * 7)(5));
 console.log(seq((x) => x + 1)((x) => x * 2)((x) => x / 3)((x) => x - 4)(7));
+
+//Додаткові приклади
+console.log(seq((x) => x + 10)((x) => x * 3)((x) => x - 5)((x) => x / 2)(8));
+console.log(seq((x) => x ** 2)((x) => x + 6)((x) => x / 2)((x) => x - 2)(10));
