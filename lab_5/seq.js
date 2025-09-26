2fa929452665424331a61e4b5d6ae854d9bc74a0
dan-gryy <dan.gryschenko@gmail.com>
2025-09-26 23:23:20 +0300

    fuction edited

--- lab_5/seq.js
+++ lab_5/seq.js
@@ -1,11 +1,17 @@
 //Функція seq приймає дві функції f і g і повертає нову функцію, яка застосовує f до свого аргументу, а потім застосовує g до результату f. Якщо g є рядком "Number", то вона перетворює результат f у число перед застосуванням g. Інакше вона просто застосовує g до результату f.
+function seq(...args) {
+  const functions = [...args];
 
-const seq = (f) => (g) => {
-  if (g === "Number") {
-    return (x) => f(g(x));
-  } else {
-    return (x) => g(f(x));
-  }
-};
-console.log(seq((x) => x + 1)((x) => x * 2)(3));
-console.log(seq((x) => x + 7)((x) => x * 2)(11));
+  const a = (...newArgs) => {
+    if (typeof newArgs[0] === "function") {
+      functions.push(...newArgs);
+      return a;
+    } else {
+      return functions.reduceRight((result, fn) => fn(result), newArgs[0]);
+    }
+  };
+  return a;
+}
+console.log(seq((x) => x + 7)((x) => x * 2)(5));
+console.log(seq((x) => x + 2)((x) => x * 7)(5));
+console.log(seq((x) => x + 1)((x) => x * 2)((x) => x / 3)((x) => x - 4)(7));
